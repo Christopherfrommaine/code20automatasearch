ed0c725b67f46c926abc3563b8a7c75809ef44b6
Christopherfrommaine <christopherfrommaine@gmail.com>
2025-03-04 15:22:05 -0500

    pattern sep test 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rayon::prelude::*;
 use std::collections::HashSet;
+use cgrustplot::plots::array_plot::array_plot;
 
 // Implements a code-20 step on a integer acting like a boolean list.
 #[inline(always)]
@@ -15,6 +16,7 @@ fn step(init: u64) -> u64 {
     (a | b | c | d | e) ^ (a ^ b ^ c ^ d ^ e)
 }
 
+// Will the initialization never halt?
 #[inline(always)]
 fn lifetimeinfinite(init: u64, max: u32) -> bool {
     let mut o = init;
@@ -26,13 +28,24 @@ fn lifetimeinfinite(init: u64, max: u32) -> bool {
     return true;
 }
 
-fn hasheduniquepatterns() {
-    let mut h: HashSet<u64> = HashSet::new();
+// Converts a u64 to a list of bits. Used for debugging
+fn u64tobits(n: u64) -> Vec<u8> {
+    let mut bits = Vec::with_capacity(64);
+    for i in (0..64).rev() {
+        bits.push(((n >> i) & 1) as u8);
+    }
+    bits
+}
 
+fn main() {
+    let mut h: HashSet<u64> = HashSet::new();  // Keeps track of seen states
+
+    // Batching for paralellization
     let batch_size: u64 = 1_000_000;
-    let mut bn: u64 = 0;
+    let mut bn: u64 = 0; // batch number
 
     loop {
+        // Paralellize filtering of halting states and filtering of seen states
         let v: Vec<u64> = ((batch_size * bn + 1)..(batch_size * (bn + 1)))
             .into_par_iter()
             .filter(|i| {
@@ -41,25 +54,26 @@ fn hasheduniquepatterns() {
 
                 lifetimeinfinite(i << 32, 500)
             })
-            .collect();
-        bn += 1;
+            .filter(|i| {
+                let mut s = i << 32;
+                for _ in 0..500 {s = step(s);}
 
+                let mut next_ten = [0u64; 10];
+                for n in 0..10 {
+                    s = step(s);
+                    next_ten[n] = s;
+                }
+
+                !next_ten.iter().any(|x| h.contains(&(x >> x.trailing_zeros())))
+            }).collect();
+        
+        // For non-halting, non-seen states:
         for i in v {
+            // Check for uniqueness AGAIN (repeated in case the hash set was updated previuosly)
             let mut s = i << 32;
             for _ in 0..500 {s = step(s);}
 
-            let mut next_ten = [0u64; 10];
-            for n in 0..10 {
-                s = step(s);
-                next_ten[n] = s;
-            }
-
-            // Also check reverse case
-            let not_unique = next_ten.iter().any(|x| h.contains(&(x >> x.trailing_zeros())));
-
-            if not_unique {continue;}
-
-            // More expensive check, but extremely rare to get this far
+            // More expensive check, but rare to get this far
             let mut next_hundred = [0u64; 100];
             for n in 0..100 {
                 s = step(s);
@@ -70,18 +84,41 @@ fn hasheduniquepatterns() {
 
             if not_unique {continue;}
 
+            if i == 678175 {
+                let d: Vec<Vec<u8>> = next_hundred.iter().map(|x| u64tobits(*x)).collect();
+                array_plot(&d).set_axes(false).print();
+            }
+
+            // Check for duplicated patterns
+            const SEP: u64 = 3; // minimum seperation
+            const SEPB: u64 = (1u64 << SEP) - 1;  // seperation bitmask
+            let mut only = true;
+            for g in 1..64 {
+                if (SEPB.rotate_left(g) & s) == 0 {
+                    let low = s & ((1 << g) - 1);
+                    let high = s & (((1 << (64 - g)) - 1) << g);
+
+                    if low != 0 && high != 0 && h.contains(&(low >> low.trailing_zeros())) || h.contains(&(high >> high.trailing_zeros())) {
+                        only = false;
+                        break;
+                    }
+                }
+            }
+
+            // Update hash list
             for f in 0..64 {
                 next_hundred.iter().for_each(|x| {
-                    h.insert(x.rotate_right(f));
-                    h.insert(x.reverse_bits().rotate_right(f));
+                    let xr = x.rotate_right(f);
+                    h.insert(xr >> xr.trailing_zeros());
+                    h.insert(xr.reverse_bits() >> xr.reverse_bits().trailing_zeros());
                 });
             }
 
-            println!("{},", i);
+            if only {
+                println!("{},", i);
+            }
         }
-    }
-}
 
-fn main() {
-    hasheduniquepatterns();
+        bn += 1;
+    }
 }
